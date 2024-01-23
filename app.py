72a691279f54c8993da33902eac0679e563bee36
farhan-fk <120705181+farhan-fk@users.noreply.github.com>
2024-01-23 20:09:57 +0530

    app.py

--- app.py
+++ app.py
@@ -26,8 +26,7 @@ def input_pdf_text(uploaded_file):
 
 input_prompt="""
 Hey Act Like a skilled or very experience ATS(Application Tracking System)
-with a deep understanding of tech field,software engineering,data science ,data analyst
-and big data engineer. Your task is to evaluate the resume based on the given job description.
+with a deep understanding of Sales & Marketing and Tech field. Your task is to evaluate the resume based on the given job description.
 You must consider the job market is very competitive and you should provide 
 best assistance for improving thr resumes. Assign the percentage Matching based 
 on Jd and
